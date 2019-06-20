a4bd63fe5f410d751920f51e0a5645504046b513
Kuchitama <kiyo.kunihira@gmail.com>
2019-06-20 16:26:14 +0900

    Modify braces

--- keyboards/fortitude60/keymaps/kuchitama/keymap.c
+++ keyboards/fortitude60/keymaps/kuchitama/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |  EN  |  JA  |   N  |   M  |   ,  |   .  |//Ctrl|RShift|
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |   =  |  GUI |Lower |Space | Ctrl |Tab |Enter |Raise |  Alt |  GUI |
+ *               |   =  |  GUI |[/Lower|Space| Ctrl |Tab   |Enter |]/Raise| Alt |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MHEN, KC_HENK, KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT, \
-                 KC_EQL,  KC_LGUI,   LOWER,  KC_SPC, KC_BSPC,  KC_TAB, KC_ENT, RAISE,    KC_RALT, KC_RGUI\
+                 KC_EQL,  KC_LGUI,   LT(LOWER,KC_LBRC),KC_SPC,KC_BSPC, KC_TAB, KC_ENT, LT(RAISE,KC_RBRC),  KC_RALT, KC_RGUI\
 ),
 
 /* Colemak
