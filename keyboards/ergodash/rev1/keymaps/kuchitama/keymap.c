87c5a24df3d59fccb2f15de545feb5ad2b49cc35
Kuchitama <kiyo.kunihira@gmail.com>
2019-08-17 13:34:08 +0900

    Update positions of Enter and Space

--- keyboards/ergodash/rev1/keymaps/kuchitama/keymap.c
+++ keyboards/ergodash/rev1/keymaps/kuchitama/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Esc |                    | Ctrl|   N  |   M  |   ,  |   .  |   /  |RShift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl | Ctrl |  GUI |  ALt | ||||||| Lower| Space| Bksp|||||||| Tab | Enter| Raise|||||||| Meta | Alt | GUI | Ctrl |
+   * | Ctrl |  GUI |  ALt | Lower| ||||||| Space| Space| Bksp|||||||| Tab |Enter|Enter|||||||| Raise| Alt | GUI | Ctrl |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MHEN,                       KC_HENK, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                        KC_RCTL, KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT, \
-    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT,             LOWER,   KC_SPC ,KC_BSPC,         KC_TAB, KC_ENT , RAISE,            KC_RGUI, KC_RALT, KC_RGUI,   KC_RCTL\
+    KC_LCTL, KC_LGUI, KC_LALT,LOWER,               KC_SPC ,KC_SPC, KC_BSPC,         KC_TAB, KC_ENT, KC_ENT ,             RAISE, KC_RALT, KC_RGUI,  KC_RCTL\
   ),
 
   /* Lower
